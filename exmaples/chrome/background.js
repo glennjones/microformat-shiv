bdf83aaefbee1acd6d6cf9d1470e982c9360e0c2
Glenn Jones <glennjonesnet@gmail.com>
2013-02-07 23:41:34 +0000

    Example Chrome extension

---
+++ exmaples/chrome/background.js
@@ -0,0 +1,22 @@
+
+var tabData = {}
+
+chrome.extension.onMessage.addListener(
+  function(message, sender, sendResponse){
+    switch (message.method) {
+
+      case "getData":
+        var data = tabData[message.tabid];
+        sendResponse(data);
+        break;
+
+      case "storeDate":
+        tabData[sender.tab.id] = {'data': message.data, 'url': message.url};
+        chrome.pageAction.show(sender.tab.id);
+        sendResponse({});
+        break;
+
+    }
+
+  }
+);
