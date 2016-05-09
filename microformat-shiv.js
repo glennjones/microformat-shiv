731edfb82f13958f1ec68294f4c1daa42203005f
Glenn Jones <glennjonesnet@gmail.com>
2016-05-09 11:13:28 +0100

    Fixed value/class date formt to html5 profile

--- microformat-shiv.js
+++ microformat-shiv.js
@@ -1,6 +1,6 @@
 /*
-   microformat-shiv - v1.4.0
-   Built: 2016-03-02 10:03 - http://microformat-shiv.com
+   microformat-shiv - v1.4.1
+   Built: 2016-05-09 11:05 - http://microformat-shiv.com
    Copyright (c) 2016 Glenn Jones
    Licensed MIT 
 */
@@ -21,7 +21,7 @@ var Microformats; // jshint ignore:line
     var modules = {};
     
 
-	modules.version = '1.4.0';
+	modules.version = '1.4.1';
 	modules.livingStandard = '2015-09-25T12:26:04Z';
 
 	/**
@@ -1064,7 +1064,8 @@ var Microformats; // jshint ignore:line
 					return out.join('');
 				}
 				if(propertyType === 'dt') {
-					return modules.dates.concatFragments(out,this.options.dateFormat).toString(this.options.dateFormat);
+					var format = (this.options.dateFormat === 'auto')? 'html5' : this.options.dateFormat;
+					return modules.dates.concatFragments(out,format).toString(format);
 				}
 			} else {
 				return null;
@@ -1466,9 +1467,9 @@ var Microformats; // jshint ignore:line
 
 	// check parser module is loaded
 	if(modules.Parser){
-	
+
 		/**
-		 * applies "implied rules" microformat output structure i.e. feed-title, name, photo, url and date 
+		 * applies "implied rules" microformat output structure i.e. feed-title, name, photo, url and date
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf (microformat output structure)
@@ -1478,40 +1479,40 @@ var Microformats; // jshint ignore:line
 		 */
 		 modules.Parser.prototype.impliedRules = function(node, uf, parentClasses) {
 			var typeVersion = (uf.typeVersion)? uf.typeVersion: 'v2';
-			
+
 			// TEMP: override to allow v1 implied properties while spec changes
 			if(this.options.impliedPropertiesByVersion === false){
 				typeVersion = 'v2';
 			}
-			
+
 			if(node && uf && uf.properties) {
-				uf = this.impliedBackwardComp( node, uf, parentClasses );  
+				uf = this.impliedBackwardComp( node, uf, parentClasses );
 				if(typeVersion === 'v2'){
 					uf = this.impliedhFeedTitle( uf );
-					uf = this.impliedName( node, uf ); 
-					uf = this.impliedPhoto( node, uf ); 	
+					uf = this.impliedName( node, uf );
+					uf = this.impliedPhoto( node, uf );
 					uf = this.impliedUrl( node, uf );
 				}
 				uf = this.impliedValue( node, uf, parentClasses );
 				uf = this.impliedDate( uf );
-				
+
 				// TEMP: flagged while spec changes are put forward
 				if(this.options.parseLatLonGeo === true){
 					uf = this.impliedGeo( uf );
-				}  
+				}
 			}
 
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * apply implied name rule
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */		
+		 */
 		modules.Parser.prototype.impliedName = function(node, uf) {
 			// implied name rule
 			/*
@@ -1529,7 +1530,7 @@ var Microformats; // jshint ignore:line
 			*/
 			var name,
 				value;
-					
+
 			if(!uf.properties.name) {
 				value = this.getImpliedProperty(node, ['img', 'area', 'abbr'], this.getNameAttr);
 				var textFormat = this.options.textFormat;
@@ -1543,27 +1544,27 @@ var Microformats; // jshint ignore:line
 					uf.properties.name = name;
 				}
 			}
-			
+
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * apply implied photo rule
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */		
+		 */
 		modules.Parser.prototype.impliedPhoto = function(node, uf) {
 			// implied photo rule
 			/*
 				img.h-x[src] 												<img class="h-card" alt="Jane Doe" src="jane.jpeg"/>
 				object.h-x[data] 											<object class="h-card" data="jane.jpeg"/>Jane Doe</object>
-				.h-x>img[src]:only-of-type:not[.h-*]						<div class="h-card"><img alt="Jane Doe" src="jane.jpeg"/></div> 
-				.h-x>object[data]:only-of-type:not[.h-*] 					<div class="h-card"><object data="jane.jpeg"/>Jane Doe</object></div> 
-				.h-x>:only-child>img[src]:only-of-type:not[.h-*] 			<div class="h-card"><span><img alt="Jane Doe" src="jane.jpeg"/></span></div> 
-				.h-x>:only-child>object[data]:only-of-type:not[.h-*] 		<div class="h-card"><span><object data="jane.jpeg"/>Jane Doe</object></span></div> 
+				.h-x>img[src]:only-of-type:not[.h-*]						<div class="h-card"><img alt="Jane Doe" src="jane.jpeg"/></div>
+				.h-x>object[data]:only-of-type:not[.h-*] 					<div class="h-card"><object data="jane.jpeg"/>Jane Doe</object></div>
+				.h-x>:only-child>img[src]:only-of-type:not[.h-*] 			<div class="h-card"><span><img alt="Jane Doe" src="jane.jpeg"/></span></div>
+				.h-x>:only-child>object[data]:only-of-type:not[.h-*] 		<div class="h-card"><span><object data="jane.jpeg"/>Jane Doe</object></span></div>
 			*/
 			var value;
 			if(!uf.properties.photo) {
@@ -1575,24 +1576,24 @@ var Microformats; // jshint ignore:line
 					}
 					uf.properties.photo = [modules.utils.trim(value)];
 				}
-			}		
+			}
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * apply implied URL rule
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */		
+		 */
 		modules.Parser.prototype.impliedUrl = function(node, uf) {
 			// implied URL rule
 			/*
 				a.h-x[href]  							<a class="h-card" href="glenn.html">Glenn</a>
 				area.h-x[href]  						<area class="h-card" href="glenn.html">Glenn</area>
-				.h-x>a[href]:only-of-type:not[.h-*]  	<div class="h-card" ><a href="glenn.html">Glenn</a><p>...</p></div> 
+				.h-x>a[href]:only-of-type:not[.h-*]  	<div class="h-card" ><a href="glenn.html">Glenn</a><p>...</p></div>
 				.h-x>area[href]:only-of-type:not[.h-*]  <div class="h-card" ><area href="glenn.html">Glenn</area><p>...</p></div>
 			*/
 			var value;
@@ -1605,24 +1606,25 @@ var Microformats; // jshint ignore:line
 					}
 					uf.properties.url = [modules.utils.trim(value)];
 				}
-			}	
+			}
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * apply implied date rule - if there is a time only property try to concat it with any date property
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */		
+		 */
 		modules.Parser.prototype.impliedDate = function(uf) {
 			// implied date rule
 			// http://microformats.org/wiki/value-class-pattern#microformats2_parsers
 			// http://microformats.org/wiki/microformats2-parsing-issues#implied_date_for_dt_properties_both_mf2_and_backcompat
 			var newDate;
 			if(uf.times.length > 0 && uf.dates.length > 0) {
+				console.log(this.options.dateFormat)
 				newDate = modules.dates.dateTimeUnion(uf.dates[0][1], uf.times[0][1], this.options.dateFormat);
 				uf.properties[this.removePropPrefix(uf.times[0][0])][0] = newDate.toString(this.options.dateFormat);
 			}
@@ -1631,8 +1633,8 @@ var Microformats; // jshint ignore:line
 			delete uf.dates;
 			return uf;
 		};
-			
-			
+
+
 		/**
 		 * get an implied property value from pre-defined tag/attriubte combinations
 		 *
@@ -1643,10 +1645,10 @@ var Microformats; // jshint ignore:line
 		 */
 		modules.Parser.prototype.getImpliedProperty = function(node, tagList, getAttrFunction) {
 			// i.e. img.h-card
-			var value = getAttrFunction(node), 
+			var value = getAttrFunction(node),
 				descendant,
 				child;
-					
+
 			if(!value) {
 				// i.e. .h-card>img:only-of-type:not(.h-card)
 				descendant = modules.domUtils.getSingleDescendantOfType( node, tagList);
@@ -1664,17 +1666,17 @@ var Microformats; // jshint ignore:line
 					}
 				}
 			}
-					
+
 			return value;
 		};
-			
-			
+
+
 		/**
 		 * get an implied name value from a node
 		 *
 		 * @param  {DOM Node} node
 		 * @return {String || null}
-		 */		
+		 */
 		modules.Parser.prototype.getNameAttr = function(node) {
 			var value = modules.domUtils.getAttrValFromTagList(node, ['img','area'], 'alt');
 			if(!value) {
@@ -1682,14 +1684,14 @@ var Microformats; // jshint ignore:line
 			}
 			return value;
 		};
-	
-	
+
+
 		/**
 		 * get an implied photo value from a node
 		 *
 		 * @param  {DOM Node} node
 		 * @return {String || null}
-		 */	
+		 */
 		modules.Parser.prototype.getPhotoAttr = function(node) {
 			var value = modules.domUtils.getAttrValFromTagList(node, ['img'], 'src');
 			if(!value && modules.domUtils.hasAttributeValue(node, 'class', 'include') === false) {
@@ -1697,62 +1699,62 @@ var Microformats; // jshint ignore:line
 			}
 			return value;
 		};
-			
-			
+
+
 		/**
 		 * get an implied photo value from a node
 		 *
 		 * @param  {DOM Node} node
 		 * @return {String || null}
-		 */		
+		 */
 		modules.Parser.prototype.getURLAttr = function(node) {
 			var value = null;
 			if(modules.domUtils.hasAttributeValue(node, 'class', 'include') === false){
-				
+
 				value = modules.domUtils.getAttrValFromTagList(node, ['a'], 'href');
 				if(!value) {
 					value = modules.domUtils.getAttrValFromTagList(node, ['area'], 'href');
 				}
-				
+
 			}
 			return value;
 		};
-		
-		
+
+
 		/**
-		 * 
+		 *
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.impliedValue = function(node, uf, parentClasses){
-			
+
 			// intersection of implied name and implied value rules
-			if(uf.properties.name) {	
+			if(uf.properties.name) {
 				if(uf.value && parentClasses.root.length > 0 && parentClasses.properties.length === 1){
 					uf = this.getAltValue(uf, parentClasses.properties[0][0], 'p-name', uf.properties.name[0]);
 				}
 			}
-			
+
 			// intersection of implied URL and implied value rules
 			if(uf.properties.url) {
 				if(parentClasses && parentClasses.root.length === 1 && parentClasses.properties.length === 1){
 					uf = this.getAltValue(uf, parentClasses.properties[0][0], 'u-url', uf.properties.url[0]);
 				}
-			}	
-			
+			}
+
 			// apply alt value
 			if(uf.altValue !== null){
 				uf.value = uf.altValue.value;
 			}
 			delete uf.altValue;
-	
-	
+
+
 			return uf;
 		};
-			
-		
+
+
 		/**
 		 * get alt value based on rules about parent property prefix
 		 *
@@ -1761,7 +1763,7 @@ var Microformats; // jshint ignore:line
 		 * @param  {String} propertyName
 		 * @param  {String} value
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.getAltValue = function(uf, parentPropertyName, propertyName, value){
 			if(uf.value && !uf.altValue){
 				// first p-name of the h-* child
@@ -1779,14 +1781,14 @@ var Microformats; // jshint ignore:line
 			}
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * if a h-feed does not have a title use the title tag of a page
 		 *
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.impliedhFeedTitle = function( uf ){
 			if(uf.type && uf.type.indexOf('h-feed') > -1){
 				// has no name property
@@ -1800,80 +1802,80 @@ var Microformats; // jshint ignore:line
 			}
 			return uf;
 		};
-		
-		
-		
+
+
+
 	    /**
 		 * implied Geo from pattern <abbr class="p-geo" title="37.386013;-122.082932">
 		 *
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.impliedGeo = function( uf ){
 			var geoPair,
 				parts,
 				longitude,
 				latitude,
 				valid = true;
-			
+
 			if(uf.type && uf.type.indexOf('h-geo') > -1){
-				
+
 				// has no latitude or longitude property
 				if(uf.properties.latitude === undefined || uf.properties.longitude === undefined ){
 
 					geoPair = (uf.properties.name)? uf.properties.name[0] : null;
 					geoPair = (!geoPair && uf.properties.value)? uf.properties.value : geoPair;
-					
+
 					if(geoPair){
 						// allow for the use of a ';' as in microformats and also ',' as in Geo URL
 						geoPair = geoPair.replace(';',',');
-						
+
 						// has sep char
 						if(geoPair.indexOf(',') > -1 ){
 							parts = geoPair.split(',');
-							
+
 							// only correct if we have two or more parts
 							if(parts.length > 1){
 
-								// latitude no value outside the range -90 or 90 
+								// latitude no value outside the range -90 or 90
 								latitude = parseFloat( parts[0] );
 								if(modules.utils.isNumber(latitude) && latitude > 90 || latitude < -90){
 									valid = false;
 								}
-								
+
 								// longitude no value outside the range -180 to 180
 								longitude = parseFloat( parts[1] );
 								if(modules.utils.isNumber(longitude) && longitude > 180 || longitude < -180){
 									valid = false;
 								}
-								
+
 								if(valid){
 									uf.properties.latitude = [latitude];
 									uf.properties.longitude  = [longitude];
 								}
 							}
-							
+
 						}
 					}
 				}
 			}
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * if a backwards compat built structure has no properties add name through this.impliedName
 		 *
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.impliedBackwardComp = function(node, uf, parentClasses){
-			
+
 			// look for pattern in parent classes like "p-geo h-geo"
 			// these are structures built from backwards compat parsing of geo
 			if(parentClasses.root.length === 1 && parentClasses.properties.length === 1) {
 				if(parentClasses.root[0].replace('h-','') === this.removePropPrefix(parentClasses.properties[0][0])) {
-					
+
 					// if microformat has no properties apply the impliedName rule to get value from containing node
 					// this will get value from html such as <abbr class="geo" title="30.267991;-97.739568">Brighton</abbr>
 					if( modules.utils.hasProperties(uf.properties) === false ){
@@ -1881,12 +1883,12 @@ var Microformats; // jshint ignore:line
 					}
 				}
 			}
-			
+
 			return uf;
 		};
-		
-		
-	
+
+
+
 	}
 
 
--- modern/microformat-shiv-modern.js
+++ modern/microformat-shiv-modern.js
@@ -1,7 +1,7 @@
 /*
    Modern
-   microformat-shiv - v1.4.0
-   Built: 2016-03-02 10:03 - http://microformat-shiv.com
+   microformat-shiv - v1.4.1
+   Built: 2016-05-09 11:05 - http://microformat-shiv.com
    Copyright (c) 2016 Glenn Jones
    Licensed MIT 
 */
@@ -22,7 +22,7 @@ var Microformats; // jshint ignore:line
     var modules = {};
     
 
-	modules.version = '1.4.0';
+	modules.version = '1.4.1';
 	modules.livingStandard = '2015-09-25T12:26:04Z';
 
 	/**
@@ -1065,7 +1065,8 @@ var Microformats; // jshint ignore:line
 					return out.join('');
 				}
 				if(propertyType === 'dt') {
-					return modules.dates.concatFragments(out,this.options.dateFormat).toString(this.options.dateFormat);
+					var format = (this.options.dateFormat === 'auto')? 'html5' : this.options.dateFormat;
+					return modules.dates.concatFragments(out,format).toString(format);
 				}
 			} else {
 				return null;
@@ -1467,9 +1468,9 @@ var Microformats; // jshint ignore:line
 
 	// check parser module is loaded
 	if(modules.Parser){
-	
+
 		/**
-		 * applies "implied rules" microformat output structure i.e. feed-title, name, photo, url and date 
+		 * applies "implied rules" microformat output structure i.e. feed-title, name, photo, url and date
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf (microformat output structure)
@@ -1479,40 +1480,40 @@ var Microformats; // jshint ignore:line
 		 */
 		 modules.Parser.prototype.impliedRules = function(node, uf, parentClasses) {
 			var typeVersion = (uf.typeVersion)? uf.typeVersion: 'v2';
-			
+
 			// TEMP: override to allow v1 implied properties while spec changes
 			if(this.options.impliedPropertiesByVersion === false){
 				typeVersion = 'v2';
 			}
-			
+
 			if(node && uf && uf.properties) {
-				uf = this.impliedBackwardComp( node, uf, parentClasses );  
+				uf = this.impliedBackwardComp( node, uf, parentClasses );
 				if(typeVersion === 'v2'){
 					uf = this.impliedhFeedTitle( uf );
-					uf = this.impliedName( node, uf ); 
-					uf = this.impliedPhoto( node, uf ); 	
+					uf = this.impliedName( node, uf );
+					uf = this.impliedPhoto( node, uf );
 					uf = this.impliedUrl( node, uf );
 				}
 				uf = this.impliedValue( node, uf, parentClasses );
 				uf = this.impliedDate( uf );
-				
+
 				// TEMP: flagged while spec changes are put forward
 				if(this.options.parseLatLonGeo === true){
 					uf = this.impliedGeo( uf );
-				}  
+				}
 			}
 
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * apply implied name rule
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */		
+		 */
 		modules.Parser.prototype.impliedName = function(node, uf) {
 			// implied name rule
 			/*
@@ -1530,7 +1531,7 @@ var Microformats; // jshint ignore:line
 			*/
 			var name,
 				value;
-					
+
 			if(!uf.properties.name) {
 				value = this.getImpliedProperty(node, ['img', 'area', 'abbr'], this.getNameAttr);
 				var textFormat = this.options.textFormat;
@@ -1544,27 +1545,27 @@ var Microformats; // jshint ignore:line
 					uf.properties.name = name;
 				}
 			}
-			
+
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * apply implied photo rule
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */		
+		 */
 		modules.Parser.prototype.impliedPhoto = function(node, uf) {
 			// implied photo rule
 			/*
 				img.h-x[src] 												<img class="h-card" alt="Jane Doe" src="jane.jpeg"/>
 				object.h-x[data] 											<object class="h-card" data="jane.jpeg"/>Jane Doe</object>
-				.h-x>img[src]:only-of-type:not[.h-*]						<div class="h-card"><img alt="Jane Doe" src="jane.jpeg"/></div> 
-				.h-x>object[data]:only-of-type:not[.h-*] 					<div class="h-card"><object data="jane.jpeg"/>Jane Doe</object></div> 
-				.h-x>:only-child>img[src]:only-of-type:not[.h-*] 			<div class="h-card"><span><img alt="Jane Doe" src="jane.jpeg"/></span></div> 
-				.h-x>:only-child>object[data]:only-of-type:not[.h-*] 		<div class="h-card"><span><object data="jane.jpeg"/>Jane Doe</object></span></div> 
+				.h-x>img[src]:only-of-type:not[.h-*]						<div class="h-card"><img alt="Jane Doe" src="jane.jpeg"/></div>
+				.h-x>object[data]:only-of-type:not[.h-*] 					<div class="h-card"><object data="jane.jpeg"/>Jane Doe</object></div>
+				.h-x>:only-child>img[src]:only-of-type:not[.h-*] 			<div class="h-card"><span><img alt="Jane Doe" src="jane.jpeg"/></span></div>
+				.h-x>:only-child>object[data]:only-of-type:not[.h-*] 		<div class="h-card"><span><object data="jane.jpeg"/>Jane Doe</object></span></div>
 			*/
 			var value;
 			if(!uf.properties.photo) {
@@ -1576,24 +1577,24 @@ var Microformats; // jshint ignore:line
 					}
 					uf.properties.photo = [modules.utils.trim(value)];
 				}
-			}		
+			}
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * apply implied URL rule
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */		
+		 */
 		modules.Parser.prototype.impliedUrl = function(node, uf) {
 			// implied URL rule
 			/*
 				a.h-x[href]  							<a class="h-card" href="glenn.html">Glenn</a>
 				area.h-x[href]  						<area class="h-card" href="glenn.html">Glenn</area>
-				.h-x>a[href]:only-of-type:not[.h-*]  	<div class="h-card" ><a href="glenn.html">Glenn</a><p>...</p></div> 
+				.h-x>a[href]:only-of-type:not[.h-*]  	<div class="h-card" ><a href="glenn.html">Glenn</a><p>...</p></div>
 				.h-x>area[href]:only-of-type:not[.h-*]  <div class="h-card" ><area href="glenn.html">Glenn</area><p>...</p></div>
 			*/
 			var value;
@@ -1606,24 +1607,25 @@ var Microformats; // jshint ignore:line
 					}
 					uf.properties.url = [modules.utils.trim(value)];
 				}
-			}	
+			}
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * apply implied date rule - if there is a time only property try to concat it with any date property
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */		
+		 */
 		modules.Parser.prototype.impliedDate = function(uf) {
 			// implied date rule
 			// http://microformats.org/wiki/value-class-pattern#microformats2_parsers
 			// http://microformats.org/wiki/microformats2-parsing-issues#implied_date_for_dt_properties_both_mf2_and_backcompat
 			var newDate;
 			if(uf.times.length > 0 && uf.dates.length > 0) {
+				console.log(this.options.dateFormat)
 				newDate = modules.dates.dateTimeUnion(uf.dates[0][1], uf.times[0][1], this.options.dateFormat);
 				uf.properties[this.removePropPrefix(uf.times[0][0])][0] = newDate.toString(this.options.dateFormat);
 			}
@@ -1632,8 +1634,8 @@ var Microformats; // jshint ignore:line
 			delete uf.dates;
 			return uf;
 		};
-			
-			
+
+
 		/**
 		 * get an implied property value from pre-defined tag/attriubte combinations
 		 *
@@ -1644,10 +1646,10 @@ var Microformats; // jshint ignore:line
 		 */
 		modules.Parser.prototype.getImpliedProperty = function(node, tagList, getAttrFunction) {
 			// i.e. img.h-card
-			var value = getAttrFunction(node), 
+			var value = getAttrFunction(node),
 				descendant,
 				child;
-					
+
 			if(!value) {
 				// i.e. .h-card>img:only-of-type:not(.h-card)
 				descendant = modules.domUtils.getSingleDescendantOfType( node, tagList);
@@ -1665,17 +1667,17 @@ var Microformats; // jshint ignore:line
 					}
 				}
 			}
-					
+
 			return value;
 		};
-			
-			
+
+
 		/**
 		 * get an implied name value from a node
 		 *
 		 * @param  {DOM Node} node
 		 * @return {String || null}
-		 */		
+		 */
 		modules.Parser.prototype.getNameAttr = function(node) {
 			var value = modules.domUtils.getAttrValFromTagList(node, ['img','area'], 'alt');
 			if(!value) {
@@ -1683,14 +1685,14 @@ var Microformats; // jshint ignore:line
 			}
 			return value;
 		};
-	
-	
+
+
 		/**
 		 * get an implied photo value from a node
 		 *
 		 * @param  {DOM Node} node
 		 * @return {String || null}
-		 */	
+		 */
 		modules.Parser.prototype.getPhotoAttr = function(node) {
 			var value = modules.domUtils.getAttrValFromTagList(node, ['img'], 'src');
 			if(!value && modules.domUtils.hasAttributeValue(node, 'class', 'include') === false) {
@@ -1698,62 +1700,62 @@ var Microformats; // jshint ignore:line
 			}
 			return value;
 		};
-			
-			
+
+
 		/**
 		 * get an implied photo value from a node
 		 *
 		 * @param  {DOM Node} node
 		 * @return {String || null}
-		 */		
+		 */
 		modules.Parser.prototype.getURLAttr = function(node) {
 			var value = null;
 			if(modules.domUtils.hasAttributeValue(node, 'class', 'include') === false){
-				
+
 				value = modules.domUtils.getAttrValFromTagList(node, ['a'], 'href');
 				if(!value) {
 					value = modules.domUtils.getAttrValFromTagList(node, ['area'], 'href');
 				}
-				
+
 			}
 			return value;
 		};
-		
-		
+
+
 		/**
-		 * 
+		 *
 		 *
 		 * @param  {DOM Node} node
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.impliedValue = function(node, uf, parentClasses){
-			
+
 			// intersection of implied name and implied value rules
-			if(uf.properties.name) {	
+			if(uf.properties.name) {
 				if(uf.value && parentClasses.root.length > 0 && parentClasses.properties.length === 1){
 					uf = this.getAltValue(uf, parentClasses.properties[0][0], 'p-name', uf.properties.name[0]);
 				}
 			}
-			
+
 			// intersection of implied URL and implied value rules
 			if(uf.properties.url) {
 				if(parentClasses && parentClasses.root.length === 1 && parentClasses.properties.length === 1){
 					uf = this.getAltValue(uf, parentClasses.properties[0][0], 'u-url', uf.properties.url[0]);
 				}
-			}	
-			
+			}
+
 			// apply alt value
 			if(uf.altValue !== null){
 				uf.value = uf.altValue.value;
 			}
 			delete uf.altValue;
-	
-	
+
+
 			return uf;
 		};
-			
-		
+
+
 		/**
 		 * get alt value based on rules about parent property prefix
 		 *
@@ -1762,7 +1764,7 @@ var Microformats; // jshint ignore:line
 		 * @param  {String} propertyName
 		 * @param  {String} value
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.getAltValue = function(uf, parentPropertyName, propertyName, value){
 			if(uf.value && !uf.altValue){
 				// first p-name of the h-* child
@@ -1780,14 +1782,14 @@ var Microformats; // jshint ignore:line
 			}
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * if a h-feed does not have a title use the title tag of a page
 		 *
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.impliedhFeedTitle = function( uf ){
 			if(uf.type && uf.type.indexOf('h-feed') > -1){
 				// has no name property
@@ -1801,80 +1803,80 @@ var Microformats; // jshint ignore:line
 			}
 			return uf;
 		};
-		
-		
-		
+
+
+
 	    /**
 		 * implied Geo from pattern <abbr class="p-geo" title="37.386013;-122.082932">
 		 *
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.impliedGeo = function( uf ){
 			var geoPair,
 				parts,
 				longitude,
 				latitude,
 				valid = true;
-			
+
 			if(uf.type && uf.type.indexOf('h-geo') > -1){
-				
+
 				// has no latitude or longitude property
 				if(uf.properties.latitude === undefined || uf.properties.longitude === undefined ){
 
 					geoPair = (uf.properties.name)? uf.properties.name[0] : null;
 					geoPair = (!geoPair && uf.properties.value)? uf.properties.value : geoPair;
-					
+
 					if(geoPair){
 						// allow for the use of a ';' as in microformats and also ',' as in Geo URL
 						geoPair = geoPair.replace(';',',');
-						
+
 						// has sep char
 						if(geoPair.indexOf(',') > -1 ){
 							parts = geoPair.split(',');
-							
+
 							// only correct if we have two or more parts
 							if(parts.length > 1){
 
-								// latitude no value outside the range -90 or 90 
+								// latitude no value outside the range -90 or 90
 								latitude = parseFloat( parts[0] );
 								if(modules.utils.isNumber(latitude) && latitude > 90 || latitude < -90){
 									valid = false;
 								}
-								
+
 								// longitude no value outside the range -180 to 180
 								longitude = parseFloat( parts[1] );
 								if(modules.utils.isNumber(longitude) && longitude > 180 || longitude < -180){
 									valid = false;
 								}
-								
+
 								if(valid){
 									uf.properties.latitude = [latitude];
 									uf.properties.longitude  = [longitude];
 								}
 							}
-							
+
 						}
 					}
 				}
 			}
 			return uf;
 		};
-		
-		
+
+
 		/**
 		 * if a backwards compat built structure has no properties add name through this.impliedName
 		 *
 		 * @param  {Object} uf
 		 * @return {Object}
-		 */	
+		 */
 		modules.Parser.prototype.impliedBackwardComp = function(node, uf, parentClasses){
-			
+
 			// look for pattern in parent classes like "p-geo h-geo"
 			// these are structures built from backwards compat parsing of geo
 			if(parentClasses.root.length === 1 && parentClasses.properties.length === 1) {
 				if(parentClasses.root[0].replace('h-','') === this.removePropPrefix(parentClasses.properties[0][0])) {
-					
+
 					// if microformat has no properties apply the impliedName rule to get value from containing node
 					// this will get value from html such as <abbr class="geo" title="30.267991;-97.739568">Brighton</abbr>
 					if( modules.utils.hasProperties(uf.properties) === false ){
@@ -1882,12 +1884,12 @@ var Microformats; // jshint ignore:line
 					}
 				}
 			}
-			
+
 			return uf;
 		};
-		
-		
-	
+
+
+
 	}
 
 
