faf91e7aec873e53a1fb647adc42c2663ecac78b
Glenn Jones <glennjonesnet@gmail.com>
2016-10-26 10:43:27 +0100

    Parse error in impliedDate rule

--- test/interface-tests/integration-test.js
+++ test/interface-tests/integration-test.js
@@ -41,6 +41,36 @@ describe('integration', function() {
 
    });
 
+
+   // parser should not error
+   // https://github.com/glennjones/microformat-node/issues/35
+   it('part class delimiter ie dt-', function(){
+
+        var expected = {
+                'items': [
+                    {
+                        'type': [
+                            'h-entry'
+                        ],
+                        'properties': {}
+                    }
+                ],
+                'rels': {},
+                'rel-urls': {}
+            },
+			options = {
+				'html': '<div class="hentry"><div class="dt-">0AM<div class="dt-">x</div></div></div>',
+				'baseUrl': 'http://example.com',
+				'dateFormat': 'html5',
+				'parseLatLonGeo': true
+			};
+
+        var result = Microformats.get(options);
+        assert.deepEqual( result, expected );
+
+   });
+
+
 });
 
 
